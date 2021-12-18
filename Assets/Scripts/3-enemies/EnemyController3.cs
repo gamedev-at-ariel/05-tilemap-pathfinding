bebc5a6d9ec90251dbe710114814c53e1ede36a2
Erel Segal-Halevi <erelsgl@gmail.com>
2021-12-18 23:27:18 +0200

    Finally, a generic state machine!

--- Assets/Scripts/3-enemies/EnemyController3.cs
+++ Assets/Scripts/3-enemies/EnemyController3.cs
@@ -6,7 +6,7 @@
 [RequireComponent(typeof(Patroller))]
 [RequireComponent(typeof(Chaser))]
 [RequireComponent(typeof(Rotator))]
-public class EnemyController3: MonoBehaviour {
+public class EnemyController3: StateMachine {
     [SerializeField] float radiusToWatch = 5f;
     [SerializeField] float probabilityToRotate = 0.2f;
     [SerializeField] float probabilityToStopRotating = 0.2f;
@@ -15,46 +15,24 @@ public class EnemyController3: MonoBehaviour {
     private Patroller patroller;
     private Rotator rotator;
 
-    private void Chase() {
-        chaser.enabled = true;
-        patroller.enabled = rotator.enabled = false;
+    private float DistanceToTarget() {
+        return Vector3.Distance(transform.position, chaser.TargetObjectPosition());
     }
 
-    private void Patrol() {
-        patroller.enabled = true;
-        chaser.enabled = rotator.enabled = false;
-    }
-
-    private void Rotate() {
-        rotator.enabled = true;
-        chaser.enabled = patroller.enabled = false;
-    }
-
-    private void Start() {
+    private void Awake() {
         chaser = GetComponent<Chaser>();
         patroller = GetComponent<Patroller>();
         rotator = GetComponent<Rotator>();
-        Patrol();
-    }
-
-    private void Update() {
-        float distanceToTarget = Vector3.Distance(transform.position, chaser.TargetObjectPosition());
-
-        if (distanceToTarget <= radiusToWatch) {
-            Chase();
-        } else if (rotator.enabled) {
-            if (Random.Range(0f,1f) < probabilityToStopRotating*Time.deltaTime) {
-                Patrol();
-            }
-        } else if (patroller.enabled) {
-            if (Random.Range(0f, 1f) < probabilityToRotate * Time.deltaTime) {
-                Rotate();
-            }
-        } else if (chaser.enabled) {
-            if (distanceToTarget > radiusToWatch) {
-                Patrol();
-            } 
-        }
+        base
+        .AddState(patroller)     // This would be the first active state.
+        .AddState(chaser)
+        .AddState(rotator)
+        .AddTransition(patroller, () => DistanceToTarget()<=radiusToWatch,   chaser)
+        .AddTransition(rotator,   () => DistanceToTarget()<=radiusToWatch,   chaser)
+        .AddTransition(chaser,    () => DistanceToTarget() > radiusToWatch, patroller)
+        .AddTransition(rotator,   () => Random.Range(0f, 1f) < probabilityToStopRotating * Time.deltaTime, patroller)
+        .AddTransition(patroller, () => Random.Range(0f, 1f) < probabilityToRotate * Time.deltaTime, rotator)
+        ;
     }
 
     private void OnDrawGizmosSelected() {
